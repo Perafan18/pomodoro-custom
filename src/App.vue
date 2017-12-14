29519bb0974c9a758fc8d71877d3615f7bc991e2
Juan Carlos Correa Herrera <juan.correa@varangard.com>
2017-12-14 13:45:07 -0600

    Add basic time logic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    h1 {{ title }}
+    h2 {{ showTime }}
 </template>
 
 <script>
@@ -9,8 +9,29 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: 'Welcome to Your Vue.js App',
+      timeout: null,
+      showTime: ''
     }
+  },
+
+  mounted () {
+    this.timeout = setInterval(() => this.getTime(), 1000)
+  },
+
+  methods: {
+    getTime () {
+      let datetime = new Date()
+      let hour = datetime.getHours()
+      let min = datetime.getMinutes()
+      let sec = datetime.getSeconds()
+
+      this.showTime = `${hour}:${min}:${sec}`
+    }
+  },
+
+  beforeDestroy () {
+    clearTimeout(this.timeout)
   }
 }
 </script>
