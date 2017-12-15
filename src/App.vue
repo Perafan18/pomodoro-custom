fc19344732099a73e3efe0a76df9a047036ef809
Juan Carlos Correa Herrera <juan.correa@varangard.com>
2017-12-15 11:35:06 -0600

    Add basic CustomComponent

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     main
       h1 Pomodoro Custom
       timer-component
+      custom-component
 </template>
 
 <script>
 import TimerComponent from './components/TimerComponent'
+import CustomComponent from './components/CustomComponent'
 
 export default {
   name: 'app',
 
   components: {
-    TimerComponent
+    TimerComponent,
+    CustomComponent
   }
 }
 </script>
---
+++ src/components/CustomComponent.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  section
+    h2 Custom
+    form
+      label(for="pomodoroTime") Pomodoro time:
+      input(v-model="pomodoroTime" type="number" id="pomodoroTime")
+      label(for="shortBreakTime") Short break time:
+      input(v-model="shortBreakTime" type="number" id="shortBreakTime")
+      label(for="longBreakTime") Long break time:
+      input(v-model="longBreakTime" type="number" id="longBreakTime")
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      pomodoroTime: 25,
+      shortBreakTime: 5,
+      longBreakTime: 10
+    }
+  },
+
+  computed: {
+    getCustom () {
+      return `
+        Pomodoro time: ${this.pomodoroTime}
+        Short break time:
+      `
+    }
+  }
+}
+</script>
